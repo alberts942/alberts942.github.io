3b64b05145eeaeed0c5ab92262bd4b639c16b98b
alberts942 <31249388+alberts942@users.noreply.github.com>
2017-12-05 15:21:30 -0700

    Update assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -1,11 +1,16 @@
+                       function getRandomInt(min, max){
+ 			min = Math.ceil(min);
+  			max = Math.floor(max);
+  			return Math.floor(Math.random() * (max - min) + min); 
+			};
 			var number = getRandomInt(0, 6);
-    		var guess = prompt("try to Guess a number between 0 and 5.", "");
+    		var guess = prompt("try to guess a number between 0 and 5.", "");
        		var guessNumber = 0;
        		while(guess != number){
-        		alert("You tried " + guess + ": The right think was" + number + ". Please try again");
+        		alert("You tried " + guess + ": The correct try was " + number + ". Please try to guess again");
           		guessNumber += 1;
           		number = getRandomInt(0,6);
-          		guess = prompt("the number is between 0 and 5.", "");
+          		guess = prompt("try to guess a number between 0 and 5.", "");
         	}
         	guessNumber += 1;
-        	alert("Your Answer " + guess + ": You right. you tried " + guessNumber + " times.");
+        	alert("You tried " + guess + ": You right. It took you " + guessNumber + " tries.");1
